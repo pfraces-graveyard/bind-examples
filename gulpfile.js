0f1ced534c650cfbe179a24ca29e91fc6c26fcd7
Pau Fracés <pfraces@gmail.com>
2015-09-03 06:49:41 +0200

    [min] fixed indentation

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,18 @@ var gulp   = require('gulp'),
     concat = require('gulp-concat-sourcemap');
 
 gulp.task('concat', function () {
-    var dependencies = [
-      './dependencies/define/src/define.js',
-      './dependencies/**/src/*.js'
-    ];
+  var dependencies = [
+    './dependencies/define/src/define.js',
+    './dependencies/**/src/*.js'
+  ];
 
-    gulp.src(dependencies)
-      .pipe(concat('dependencies.js', { sourcesContent: true }))
-      .pipe(gulp.dest('./build/'));
+  gulp.src(dependencies)
+    .pipe(concat('dependencies.js', { sourcesContent: true }))
+    .pipe(gulp.dest('./build/'));
 
-    gulp.src('./src/**/*.js')
-      .pipe(concat('app.js', { sourcesContent: true }))
-      .pipe(gulp.dest('./build'));
+  gulp.src('./src/**/*.js')
+    .pipe(concat('app.js', { sourcesContent: true }))
+    .pipe(gulp.dest('./build'));
 });
 
 gulp.task('copy', function () {
