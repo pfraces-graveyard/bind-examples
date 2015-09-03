9aa194ea2cc3a862230124775f98c1ba468843a8
Pau Fracés <pfraces@gmail.com>
2015-09-03 06:27:07 +0200

    working build system

---
+++ README.md
@@ -0,0 +1,13 @@
+bind-examples
+=============
+
+```
+git clone https://github.com/pfraces-wip/bind-examples
+cd bind-examples
+
+npm install
+udm
+
+npm run dev
+xdg-open http://localhost:3000
+```
--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,20 @@ var gulp   = require('gulp'),
 
 gulp.task('concat', function () {
     var dependencies = [
-      './dependencies/define/src/define.js'
+      './dependencies/define/src/define.js',
+      './dependencies/**/src/*.js'
     ];
 
     gulp.src(dependencies)
-      .pipe(concat('dependencies.js'))
+      .pipe(concat('dependencies.js', { sourcesContent: true }))
       .pipe(gulp.dest('./build/'));
+
+    gulp.src('./src/**/*.js')
+      .pipe(concat('app.js', { sourcesContent: true }))
+      .pipe(gulp.dest('./build'));
+});
+
+gulp.task('copy', function () {
+  gulp.src(['./src/index.html', './src/styles.css'])
+    .pipe(gulp.dest('./build'));
 });
